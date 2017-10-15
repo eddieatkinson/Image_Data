2db030ba2ee7ddb921ffd8ff635c51471b451b78
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-10-15 16:45:50 -0400

    Image data

--- js/CPQ_image_data.js
+++ js/CPQ_image_data.js
@@ -4,10 +4,17 @@ $(document).ready(()=>{
 	$('#get-files').on('change', function(){
 		// for(let i = 0; i < ('#get-files'.value.length); i++){
 			// file = inp.files;
-			var name = $(this).val().split(/[\\/]/);
+			var name_all_chars = $(this).val().split(/[\\/]/);
+			var name_1_char = $(this).val().split(/[\\]/);
+			var name_none = $(this).val().split();
 			
 			// name.split('.');
-			console.log(name[name.length - 1]);
+			console.dir($(this));
+			// console.log($(this).value());
+			// console.log($(this).files());
+			console.log(name_all_chars);
+			console.log(name_1_char);
+			console.log(name_none);
 
 			// console.log($('#get-files').input);
 		// }
